23fadc80d9d8638e76eec4017023822152b8819b
II03uTuB <sir.med2012@gmail.com>
2020-09-19 20:05:48 +0300

    class consumer

---
+++ src/synchronizators/semaphore/Consumer.java
@@ -0,0 +1,17 @@
+package synchronizators.semaphore;
+
+public class Consumer implements Runnable {
+    QueueClass queueClass;
+
+    public Consumer(QueueClass queueClass) {
+        this.queueClass = queueClass;
+        new Thread(this, "Consumer").start();
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            queueClass.get();
+        }
+    }
+}
