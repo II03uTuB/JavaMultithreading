1c72c1d161433ef83ed491ecb084c636507b5c73
II03uTuB <sir.med2012@gmail.com>
2020-09-19 20:08:09 +0300

    class producer

---
+++ src/synchronizators/semaphore/Producer.java
@@ -0,0 +1,17 @@
+package synchronizators.semaphore;
+
+public class Producer implements Runnable {
+    QueueClass queueClass;
+
+    public Producer(QueueClass queueClass) {
+        this.queueClass = queueClass;
+        new Thread(this, "Consumer").start();
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            queueClass.put(i);
+        }
+    }
+}
