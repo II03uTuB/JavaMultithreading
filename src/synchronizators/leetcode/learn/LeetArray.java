2df72352c81a8a11f91a288d0ca98ea7a339fee4
II03uTuB <sir.med2012@gmail.com>
2020-09-22 20:01:30 +0300

    new solution

--- src/synchronizators/leetcode/learn/LeetArray.java
+++ src/synchronizators/leetcode/learn/LeetArray.java
@@ -2,31 +2,13 @@ package synchronizators.leetcode.learn;
 
 public  class LeetArray {
     public static int pivotIndex(int[] nums) {
-        int leftSum = 0;
-        int center = -1;
-        if(nums.length == 0) {
-            return -1;
+        int sum = 0, leftsum = 0;
+        for (int x: nums) sum += x;
+        for (int i = 0; i < nums.length; ++i) {
+            if (leftsum == sum - leftsum - nums[i]) return i;
+            leftsum += nums[i];
         }
-        for (int i = -1; i < nums.length; i++) {
-            if (i == -1){
-                leftSum += 0;
-            }
-            else{
-                leftSum += nums[i];
-            }
-            int rightSum = 0;
-            for (int j = nums.length - 1; j > i ; j--) {
-                rightSum += nums[j];
-                System.out.println(i + " " + j);
-                if (leftSum == rightSum) {
-                    if(j == i + 2) {
-                        center = i + 1;
-                        break;
-                    }
-                }
-            }
-        }
-        return center;
+        return -1;
     }
 
 
