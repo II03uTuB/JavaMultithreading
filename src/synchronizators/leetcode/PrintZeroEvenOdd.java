7a0566db433d44939e7d010c753a1bd9d5083bfa
II03uTuB <sir.med2012@gmail.com>
2020-09-22 18:09:00 +0300

    add constructor

--- src/synchronizators/leetcode/PrintZeroEvenOdd.java
+++ src/synchronizators/leetcode/PrintZeroEvenOdd.java
@@ -1,4 +1,38 @@
 package synchronizators.leetcode;
 
+import java.util.function.IntConsumer;
+
 public class PrintZeroEvenOdd {
+    private int n;
+
+    public PrintZeroEvenOdd(int n) {
+        this.n = n;
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                zero();
+            }
+        })
+
+    }
+
+
+    public void zero(IntConsumer printNumber) throws InterruptedException {
+
+    }
+
+    public void even(IntConsumer printNumber) throws InterruptedException {
+
+    }
+
+    public void odd(IntConsumer printNumber) throws InterruptedException {
+
+    }
+
+    public static void main(String[] args) {
+        int n = 32;
+        PrintZeroEvenOdd print  = new PrintZeroEvenOdd(n);
+        print.
+
+    }
 }
