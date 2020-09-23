27f23ee49a40e99879217502eaebd4cc4e544fbb
II03uTuB <sir.med2012@gmail.com>
2020-09-23 20:34:01 +0300

    write code to containsDublicate

---
+++ src/synchronizators/leetcode/learn/ContainsDublucate.java
@@ -0,0 +1,14 @@
+package synchronizators.leetcode.learn;
+
+public class ContainsDublucate {
+    public static boolean containsDuplicate(int[] nums) {
+        for(int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++){
+                if(nums[j] == nums[i]){
+                    return true;
+                }
+            }
+        }
+            return false;
+    }
+}
